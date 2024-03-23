21ae8fd53a02bc80050672b6d1ec3c037012aa84
pdev <kangserpobtsuasvaaj@gmail.com>
2024-03-22 20:52:29 -0700

    Spring Boot School

---
+++ src/main/java/com/alex/sms/security/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.alex.sms.security;
+
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class SecurityConfig.
+ * https://docs.spring.io/spring-security/site/docs/current/guides/html5/hellomvc-javaconfig.html
+ */
+
+public class SecurityConfig {
+
+   
+}
